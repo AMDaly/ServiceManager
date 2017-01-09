9a92f0c917fb257d2bbe64e6ac9f08f3caa2d748
amdaly <amdaly625@gmail.com>
2017-01-09 16:15:12 -0500

    Log4Net. Refactor InitialServiceScan to ServiceScan.cs.

---
+++ PeekServiceMonitor/Commands/ServiceScan.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PeekServiceMonitor.ViewModel;
+using log4net;
+using System.Windows.Input;
+
+namespace PeekServiceMonitor.Commands
+{
+    class ServiceScan : WpfCommandBase<MainWindowViewModel>
+    {
+        List<String> initialSvcList = new List<String>();
+        private readonly ILog logger;
+
+        public ServiceScan()
+        {
+            logger = LogManager.GetLogger(typeof(ServiceScan));
+        }
+
+        protected override void ExecuteInternal(MainWindowViewModel parameter)
+        {
+
+            logger.Info("Inside ExecuteServiceScan");
+
+            initialSvcList.Add("PeekStateMachineService_V1.7.1");
+            initialSvcList.Add("PeekSchedulerService_V1.7.1");
+
+            Task.Run(() =>
+            {
+                foreach (var svc in initialSvcList)
+                {
+                    try
+                    {
+                        logger.Info("Adding Service..");
+                        parameter.Add(new ServiceRunningViewModel(svc));
+                        logger.Info("Service added.");
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.Warn(string.Format("Failed to add Service: {0}.", svc), ex);
+                        logger.Warn(ex.InnerException);
+                    }
+                }
+            }).Wait();
+        }
+    }
+}
